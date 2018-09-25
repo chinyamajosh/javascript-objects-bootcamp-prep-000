579f7a143aa4debb4a1f336c0ca27193c5fc667d
Joshua Chinyama <chinyamajosh@gmail.com>
2018-09-25 09:55:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,8 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = 'My Bloody Valentine';
+  
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
